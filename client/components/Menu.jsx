bcc895c95567decb11860d2c50b98a0e5699f29b
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-10 11:44:24 +1200

    added random styling to start layout testing

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -9,19 +9,19 @@ const Menu = () => {
         <div className='menuContainer'>
             <div className='menuHeaderWrapper'>
                 <Link to='/fashion' className='menuHeader'>
-                    <h1>Fashion</h1>
+                    <h1 id='menuHeaderStyling'>Fashion</h1>
                 </Link>
                 <Link to='/travel' className='menuHeader'>
-                    <h1>Travel</h1>
+                    <h1 id='menuHeaderStyling'>Travel</h1>
                 </Link>
                 <Link to='/documentary' className='menuHeader'>
-                    <h1>Documentary</h1>
+                    <h1 id='menuHeaderStyling'>Documentary</h1>
                 </Link>
                 <Link to='/about' className='menuHeader'>
-                    <h1>About</h1>
+                    <h1 id='menuHeaderStyling'>About</h1>
                 </Link>
                 <Link to='/' className='menuHeader'>
-                    <button>Home</button>
+                    <button className='globalHomeBtn'>Home</button>
                 </Link>
             </div>   
         </div>
