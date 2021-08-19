72871a17081fed2b6b291cf4cd25ecd31b699ea1
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-19 21:17:57 +1200

    added photo compnonent with header

---
+++ client/components/Photo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Photo = () => {
+
+
+    return (
+        <>
+            <div className='photo-container'>
+                <h1 className='photo-header'>Photography</h1>
+            </div>
+        </>
+    )
+}
+
+export default Photo
