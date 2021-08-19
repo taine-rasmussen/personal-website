b850ca9a00a28c79c9e7dae3214283e58de34795
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-20 00:43:14 +1200

    added emailjs & created form - no styling

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { sendForm } from 'emailjs-com'
 
 const Contact = () => {
 
+    const sendEmail = (e) => {
+        e.preventDefault()
+        sendForm("service_vfkw5ts", "contact_form", e.target, "user_07A1Lkmj3NdwJpVweNfNQ")
+          .then(result => {
+            console.log('Email sent successfully!')
+          }, error => {
+            console.log(error.text)
+          })
+      }
+
     return(
+      
         <section className='contact-container'>
             <div>
                 <h1 className='contact-header'
@@ -10,7 +22,34 @@ const Contact = () => {
                 data-aos-duration={1500}
                 data-aos-delay={100}>Contact</h1>
             </div>
+
+            <div>
+                <form onSubmit={sendEmail}>
+
+                    <label className="label">Name</label>
+                    <input type="text" name="contact_number" />
+
+                        <br></br>
+
+                    <label className="label">Email</label>
+                    <input className="input" type="email" name="user_email"/>
+
+                        <br></br>
+
+                    <label className="label">Message</label>
+                    <textarea className="textarea" name="message"></textarea>
+
+                        <br></br>
+
+                    <button>Submit</button>
+                </form>
+            </div>
+
+
+ 
+
         </section>
+    
     )
 }
 
