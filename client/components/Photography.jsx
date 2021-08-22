214a481f9d17259a46fadca14cd0274bc786ce4b
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-22 23:07:32 +1200

    added 4 imgs with basic sizing / styling to photography route

--- client/components/Photography.jsx
+++ client/components/Photography.jsx
@@ -5,7 +5,24 @@ const photography = () => {
 
     return(
         <section>
-            <h1>Photogrpahy working</h1>
+            <h1>photography working</h1>
+
+        
+
+            <div className='photography-container'>
+                <div className="photography-img">
+                    <img src='Imgs/Bri1.jpg' alt='Bri1' className='photography-img'/>     
+                </div>
+                    <div className="photography-img">
+                        <img src='Imgs/Bri2.jpg' alt='Bri2' className='photography-img'/>        
+                </div>
+                    <div className="photography-img">
+                        <img src='imgs/Bri3.jpg' alt='Bri3' className='photography-img'/>
+                </div>
+                    <div className="photography-img">
+                        <img src='imgs/Bri4.jpg' alt='Bri4' className='photography-img'/>
+                </div>
+            </div>
         </section>
     )
 }
