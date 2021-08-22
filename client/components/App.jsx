84780454a6d062a95a68aa0e0f2e0d033077f6e3
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-22 22:32:58 +1200

    added photography route and button linking to page from photo component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,6 +10,7 @@ import Photo from './Photo'
 import Tech from './Tech'
 import Contact from './Contact'
 import Footer from './Footer'
+import Photography from './Photography'
 
 // TODO:
 // NavBar switches colours depeding on the bg colour of the current component
@@ -26,6 +27,7 @@ function App () {
         <Route exact path='/' component={Tech} />
         <Route exact path='/' component={Contact} />
         <Route exact path='/' component={Footer} />
+        <Route exact path='/photography' component={Photography} />
       </div>
   )
 }
---
+++ client/components/Photography.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const photography = () => {
+
+    return(
+        <section>
+            <h1>Photogrpahy working</h1>
+        </section>
+    )
+}
+
+export default photography
