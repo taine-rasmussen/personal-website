ee37e4d26a7668dc288f325bf53d1926e237ec8b
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-24 11:48:51 +1200

    set global styling for p text on both bg colours

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -21,12 +21,12 @@ const About = () => {
                     data-aos-delay={100}
                 >
                     <div className='about-left-text'>
-                        <p className='about-text'>
+                        <p>
                         I am a software developer with a desire and passion for all things creative, from design, photography, music and problem solving. Channeling all I have learnt from these interests and hobbies into my journey through Enspiral Dev Academy and into the tech industry has been the best decision I have ever made.
                         </p>
                     </div>
                     <div className='about-right-text'>
-                        <p className='about-text'>
+                        <p>
                         I am curious, love learning how things work and teaching where I can. These interests have me working on projects across multiple mediums where I can have opportunities for creative expression / solutions, produce intuitive user experience and see my work having a positive impact for those around me.
                         </p>
                     </div>
