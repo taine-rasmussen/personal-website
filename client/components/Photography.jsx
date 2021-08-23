4d3f56f77980a0da0ba7a80acc5f86c8906d6d12
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-23 23:46:44 +1200

    added 4 init imgs for travel & docu w/ placeholder flex-wrap styling

--- client/components/Photography.jsx
+++ client/components/Photography.jsx
@@ -14,19 +14,33 @@ const photography = () => {
         
 
             <div className='photography-container'>
-                <div className="photography-img">
-                    <img src='Imgs/Bri1.jpg' alt='Bri1' className='photography-img'/>     
-                </div>
-                    <div className="photography-img">
-                        <img src='Imgs/Bri2.jpg' alt='Bri2' className='photography-img'/>        
-                </div>
-                    <div className="photography-img">
-                        <img src='imgs/Bri3.jpg' alt='Bri3' className='photography-img'/>
-                </div>
-                    <div className="photography-img">
-                        <img src='imgs/Bri4.jpg' alt='Bri4' className='photography-img'/>
-                </div>
-            </div>
+                <h1>Portrait</h1>
+                    <div className="photography-img portrait">
+                        <img src='Imgs/Bri1.jpg' alt='Bri1' className='photography-img'/>     
+                    </div>
+                        <div className="photography-img portrait">
+                            <img src='Imgs/Bri2.jpg' alt='Bri2' className='photography-img'/>        
+                    </div>
+                        <div className="photography-img portrait">
+                            <img src='imgs/Bri3.jpg' alt='Bri3' className='photography-img'/>
+                    </div>
+                        <div className="photography-img portrait">
+                            <img src='imgs/Bri4.jpg' alt='Bri4' className='photography-img'/>
+                    </div>
+                <h1>Travel & documentary</h1>
+                    <div className="photography-img landscape">
+                            <img src='Imgs/travel1.jpg' alt='bri1' className='photography-img'/>     
+                        </div>
+                            <div className="photography-img portrait">
+                                <img src='Imgs/travel2.jpg' alt='Bri2' className='photography-img'/>        
+                        </div>
+                            <div className="photography-img portrait">
+                                <img src='imgs/travel3.jpg' alt='Bri3' className='photography-img'/>
+                        </div>
+                            <div className="photography-img landscape">
+                                <img src='imgs/travel4.jpg' alt='Bri4' className='photography-img'/>
+                        </div>
+                    </div>
         </section>
     )
 }
