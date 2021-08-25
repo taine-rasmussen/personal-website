e6e3e08c1fdccc93c4bdf0095c618bcd0e4de459
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-25 14:33:44 +1200

    added list of skills into tech component

--- client/components/Tech.jsx
+++ client/components/Tech.jsx
@@ -11,6 +11,40 @@ const Tech = () => {
                 data-aos='fade-down'
                 data-aos-duration={1500}
                 data-aos-delay={100}>Tech</h1>
+
+                <div className='tech-label-container'>
+                    <label className='tech-label'>JavaScript</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>HTML</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>CSS</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>React</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>Redux</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>Node.js</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>Jest</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>SuperTest</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>SuperAgent</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>Express</label>
+                </div>
+                <div className='tech-label-container'>
+                    <label className='tech-label'>SQLite with Knex</label>
+                </div>
             </section>
         </>
     )
