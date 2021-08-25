cd1333fa8faf7c3bd419a13f37ac121c29720d45
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-26 00:02:59 +1200

    added labels, blurb & header to tech / basic styling

--- client/components/Tech.jsx
+++ client/components/Tech.jsx
@@ -7,43 +7,64 @@ const Tech = () => {
     return (
         <>
             <section className='tech-container'>
-                <h1
-                data-aos='fade-down'
-                data-aos-duration={1500}
-                data-aos-delay={100}>Tech</h1>
+                <div className='tech-header-container'>
+                    <h1
+                    data-aos='fade-down'
+                    data-aos-duration={1500}
+                    data-aos-delay={100}>Tech</h1>
 
-                <div className='tech-label-container'>
-                    <label className='tech-label'>JavaScript</label>
+                    <p className='tech-blurb'> 
+                        As I continue to build these skills I am    constanlty find new rabbit holes to fall down, having fun exploring new technology & working on projects that im definitely going to finish before starting something new. Feel free to reach out & ask about any of these and how I've applied them to my projects.   
+                    </p>
                 </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>HTML</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>CSS</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>React</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>Redux</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>Node.js</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>Jest</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>SuperTest</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>SuperAgent</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>Express</label>
-                </div>
-                <div className='tech-label-container'>
-                    <label className='tech-label'>SQLite with Knex</label>
+
+
+                    <div className='tech-label-container'>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>JavaScript</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>HTML</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>CSS</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>React</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>Redux</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>Node</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>Jest</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>SuperTest</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>SuperAgent</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>Express</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>SQLite</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>Knex</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>Git Version Control</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>RESTful API's</label>
+                        </div>
+                        <div className='tech-label-single'>
+                            <label className='tech-label'>Browser & Server Debugging</label>
+                    </div>
                 </div>
             </section>
         </>
