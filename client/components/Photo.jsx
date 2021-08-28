1fe9b95666ceaf10a87af0c59391670b626a792d
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-28 21:07:20 +1200

    refactored email-message styling so input is large and text starts from top left

--- client/components/Photo.jsx
+++ client/components/Photo.jsx
@@ -8,10 +8,15 @@ const Photo = () => {
     return (
         <>
             <section className='photo-container'>
-                <h1 className='green'
-                data-aos='fade-down'
+                <h1 className='postioned-top-left'
+                data-aos='fade-right'
                 data-aos-duration={1500}
                 data-aos-delay={100}>Photography</h1>
+
+            <div className='photo-headers-container'>
+                <h2 className='photo-sub-header'>Portrait</h2>
+                <h2 className='photo-sub-header'>Street</h2>
+            </div>
                 
                 <Link to='photography'><button className='is-btn'>to photos</button></Link>
             </section>
