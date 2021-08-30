a6b76a84a24f5e9d30b4685c87bf83696be8866e
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-30 21:02:17 +1200

    set up html for portrait component

--- client/components/Portrait.jsx
+++ client/components/Portrait.jsx
@@ -8,6 +8,28 @@ const Portrait = () => {
         <> 
             <h1>Portrait working</h1>
 
+                <div className='portrait-container'>
+                    <div className='portrait-img-container'>
+                        <img src='/Imgs/street2.jpg' alt='' className='portrait-img' />
+                    </div>
+                    <div className='portrait-img-container'>
+                        <img src='/Imgs/travel1.jpg' alt='' className='landscape-img'/>
+                    </div>
+                    <div className='portrait-img-container'>
+                        <img src='/Imgs/travel2.jpg' alt='' className='portrait-img'/>
+                    </div>
+                    <div className='portrait-img-container'>
+                        <img src='/Imgs/travel3.jpg' alt='' className='portrait-img'/>
+                    </div>
+                    <div className='portrait-img-container'>
+                        <img src='/Imgs/travel4.jpg' alt='' className='landscape-img'/>
+                    </div>
+                    <div className='portrait-img-container'>
+                        <img src='/Imgs/street1.jpg' alt='' className='portrait-img'/>
+                    </div>
+                </div>
+
+
 
             <Link to='/'> 
                 <button className='is-btn'>Home</button>
