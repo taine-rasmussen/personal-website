df568b5a612925bc8b286e7a1763575b32400156
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-30 14:37:58 +1200

    refactored divs / classNames in photo component after adding imgs / set placholder text for blurb

--- client/components/Photo.jsx
+++ client/components/Photo.jsx
@@ -8,19 +8,31 @@ const Photo = () => {
     return (
         <>
             <section className='photo-container'>
+
                 <h1 className='photo-header postioned-top-left'
                 data-aos='fade-right'
                 data-aos-duration={1500}
                 data-aos-delay={100}>Photography</h1>
 
+                <p className='photo-blurb'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore repudiandae ipsam in, corporis similique fugit totam molestias tempora tempore rerum animi. Alias id distinctio voluptatibus illum commodi qui laborum. Quam!
+                </p>
+
             <div className='photo-headers-container'>
-                <Link to='photography'>
-                    <h2 className='photo-sub-header'>Portrait</h2>
-                    <h2 className='photo-sub-header'>Street</h2>
-                </Link>
+                <div className='photo-sub-header-container'>
+                    <Link to='photography'>
+                        <h2 className='photo-sub-header'>Portrait</h2>
+                        <img src='Imgs/Bri1.jpg' alt='portrait of women' className='preview-photo' />
+                    </Link>
+                </div>
+                <div className='photo-sub-header-container'>
+                    <Link to='photography'>
+                        <h2 className='photo-sub-header'>Street</h2>
+                        <img src='Imgs/travel3.jpg' alt='man walking' className='preview-photo' />
+                    </Link>
+                </div>
             </div>
-                
-               
+                   
             </section>
         </>
     )
