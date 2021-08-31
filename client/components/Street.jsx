de1c14dc1a5a3974108446c47c6fc3e8ffc2da57
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-31 15:58:28 +1200

    added func to handle flipping state on btn click / both views working and rendering

--- client/components/Street.jsx
+++ client/components/Street.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
@@ -9,14 +9,20 @@ import Preview from './Preview'
 
 const Street = () => {
 
+    const [view, setView] = useState(true)
+
+    // Handling state change on btn click
+    const changeView = () => {
+      view ? setView(false) : setView(true)
+    }
 
     return(
         <> 
             <h1>Street working</h1>
 
-        
+                <button onClick={changeView} >test</button>
 
-            
+                {view ? <Gallery /> : <Preview />}
 
             <Link to='/'> 
                 <button className='is-btn'>Home</button>
