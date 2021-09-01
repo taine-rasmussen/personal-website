d72d3fed499bd189a89143c66ac880d91eedeacd
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-02 00:38:13 +1200

    wrote function to handle chaning header on state change

--- client/components/Street.jsx
+++ client/components/Street.jsx
@@ -8,12 +8,22 @@ import Preview from './Preview'
 const Street = () => {
 
     const [view, setView] = useState(true)
+    const [header, setHeader] = useState('Gallery')
+
+
+
 
     // Handling state change on btn click
     const changeView = () => {
-      view ? setView(false) : setView(true)
+      view ? setView(false) : setView(true) 
+      changeHeader()
     }
-
+    // Handles changing page header to match gallery or preview
+    const changeHeader = () => {
+        if( header === 'Gallery'){
+            setHeader('Preview')
+        } else (setHeader('Gallery'))
+    }
 
 
         /// social icons top right?
@@ -21,12 +31,12 @@ const Street = () => {
         <>
                 <div className='street-container'> 
                     <div className='street-margin-top-container'>
-                        <h1 className='street-header'>Street working</h1>
+                        <h1 className='street-header'>{header}</h1>
                     </div>
 
                     <div className='street-margin-side-container'>
                         <div>
-                            <input type="checkbox" id="toggle" onClick={changeView}/>
+                            <input type="checkbox" id="toggle" onClick={changeView} />
                             <label for="toggle"></label>
                         </div>
                             <Link to='/'> 
