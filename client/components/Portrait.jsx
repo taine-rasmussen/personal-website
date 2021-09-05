108ed48079a013109ddf6dca787dc07aa814ce6d
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-05 17:05:21 +1200

    removed social links from portrait component

--- client/components/Portrait.jsx
+++ client/components/Portrait.jsx
@@ -11,44 +11,40 @@ const Portrait = () => {
     const [view, setView] = useState(true)
     const [header, setHeader] = useState('Portrait Gallery')
 
+    // Handles changing page header to match gallery or preview
+    const changeHeader = () => {
+        header === Gallery ? setHeader(Preview) : setHeader(Gallery)
+    }
+
     // Handling state change on btn click
     const changeView = () => {
       view ? setView(false) : setView(true) 
       changeHeader()
     }
 
-    // Handles changing page header to match gallery or preview
-    const changeHeader = () => {
-        if( header === 'Portrait Gallery'){
-            setHeader('Portrait Preview')
-        } else (setHeader('Portrait Gallery'))
-    }
     
     // build links to all sections of website using that DOM manipulation used in scroll btn
     // Landing background set to image of guy walking in paris?
     // using images to fill empty places on webpage - overlapping component borders would be nice.
     return(
         <>
-                <div className='street-container'> 
-                    <div className='street-margin-top-container'>
-                        <h1 className='street-header'
+                <div className='gallery-container'> 
+                    <div className='gallery-margin-top-container'>
+                        <h1 className='gallery-header'
                             data-aos='fade-right'
                             data-aos-duration={1500}
                             data-aos-delay={100}>{header}</h1>
                     </div>
-                    <div className='street-margin-side-container'>
+                    <div className='gallery-margin-side-container'>
                         <div>
                             <input type="checkbox" id="toggle" onClick={changeView} />
-                            <label htmlFor="toggle" className='street-label'></label>
+                            <label htmlFor="toggle" className='gallery-label'></label>
                         </div>
-                            <div className='street-social-container'>
-                                 <Footer />
-                            </div>
                             <Link to='/'> 
-                                <h2 className='street-sub-header'>Home</h2>
+                                <h2 className='gallery-sub-header'>Home</h2>
                             </Link>
                             <Link to='/street'> 
-                                <h2 className='street-sub-header'>Street</h2>
+                                <h2 className='gallery-sub-header'>Street</h2>
                             </Link>
                     </div>
                 </div>
