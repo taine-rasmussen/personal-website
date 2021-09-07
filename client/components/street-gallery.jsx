d40bb970615bf690bbe675e638726c9f277c7b22
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-07 20:20:18 +1200

    refactored street img things after changing file names

--- client/components/street-gallery.jsx
+++ client/components/street-gallery.jsx
@@ -6,22 +6,22 @@ const Streetgallery = () => {
         <>
             <div className='gallery-container'>
                 <div className='gallery-img-container'>
-                    <img src='/Imgs/street2.jpg' alt='' className='gallery-portrait' />
+                    <img src='/Imgs/street1.jpg' alt='' className='gallery-portrait' />
                 </div>
                 <div className='gallery-img-container'>
-                    <img src='/Imgs/travel1.jpg' alt='' className='gallery-landscape'/>
+                    <img src='/Imgs/street3.jpg' alt='' className='gallery-landscape'/>
                 </div>
                 <div className='gallery-img-container'>
-                    <img src='/Imgs/travel2.jpg' alt='' className='gallery-portrait'/>
+                    <img src='/Imgs/street2.jpg' alt='' className='gallery-portrait'/>
                 </div>
                 <div className='gallery-img-container'>
-                    <img src='/Imgs/travel3.jpg' alt='' className='gallery-portrait'/>
+                    <img src='/Imgs/street4.jpg' alt='' className='gallery-portrait'/>
                 </div>
                 <div className='gallery-img-container'>
-                    <img src='/Imgs/travel4.jpg' alt='' className='gallery-landscape'/>
+                    <img src='/Imgs/street6.jpg' alt='' className='gallery-landscape'/>
                 </div>
                 <div className='gallery-img-container'>
-                    <img src='/Imgs/street1.jpg' alt='' className='gallery-portrait'/>
+                    <img src='/Imgs/street5.jpg' alt='' className='gallery-portrait'/>
                 </div>
             </div>
         </>
