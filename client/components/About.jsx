d559d84742bc80367a36a7374644515cb7512fb3
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-09 19:15:50 +1200

    changed text in about component

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -14,12 +14,12 @@ const About = () => {
                 <div className='about-text-container' data-aos="fade-up" data-aos-duration={1500} data-aos-delay={100}>
                     <div className='about-left-text'>
                         <p>
-                            I am a software developer with a desire and passion for all things creative. From design, photography, music and problem solving.
+                        I am a software developer and photographer with a desire and passion for all things creative, from design, photography, music and of course programming.
                         </p>
                     </div>
                     <div className='about-right-text'>
                         <p>
-                            I am curious, love learning how things work and teaching where I can. These interests have me working on projects across multiple mediums where I can have opportunities for creative expression / solutions, produce intuitive user experience and see my work having a positive impact.
+                        Drawing me towards these different worlds is a love for a creative medium that allows for endless rabbit holes to fill any & all curious itches, while having no end destination to reach. Without the finish line in sight I get to constantly be free, learning more, exploring new ideas, challenging old ones & creating work I love.
                         </p>
                     </div>
                 </div>
