e430857e8d4a092fb7673a2f72ab3c92d1dd7c23
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-11 14:38:40 +1200

    added img to about component

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const About = () => {
 
     return(
-        <> 
+        <div> 
+
+            <img src="Imgs/street5.jpg" alt='Man walking' className='about-img'/>
+
             <section className='about-container' id='testing' >
 
                 <div className='about-header-container'>
@@ -13,18 +16,18 @@ const About = () => {
                 </div>
                 <div className='about-text-container' data-aos="fade-up" data-aos-duration={1500} data-aos-delay={100}>
                     <div className='about-left-text'>
-                        <p>
+                        <p className='bold-p'>
                         I am a software developer and photographer with a desire and passion for all things creative, from design, photography, music and of course programming.
                         </p>
                     </div>
                     <div className='about-right-text'>
-                        <p>
+                        <p className='bold-p'>
                         Drawing me towards these different worlds is a love for a creative medium that allows for endless rabbit holes to fill any & all curious itches, while having no end destination to reach. Without the finish line in sight I get to constantly be free, learning more, exploring new ideas, challenging old ones & creating work I love.
                         </p>
                     </div>
                 </div>
             </section>
-        </>
+        </div>
     )
 }
 
